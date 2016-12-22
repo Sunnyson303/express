40f1471fc3c78908009bbebf78eb4c8bf12a25f9
Sunnyson303 <260851801@qq.com>
2016-12-22 10:41:59 +0800

    update

--- test.js
+++ test.js
@@ -1,7 +1,24 @@
 import express from './index'
 import http from 'http'
+import koa from 'koa'
 
-const app = http.createServer(new express())
+// const app = new express()
+const app = new koa()
+app.use((ctx, next) => {
+   console.log('this is first middleware')
+   next()
+})
+
+app.use((ctx, next) => {
+   console.log('this is second middleware')
+  //  ctx.body = 'hello world'
+})
+
+// const app = http.createServer((req, res) => {
+//   res.end('hello world')
+// })
+
 app.listen('4000')
 
-console.log('app listen port: ', 4000);
+
+console.log('app listen port: ', 4000);  
